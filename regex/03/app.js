d91250951e0924ded3e70448fdac89cfc04c4af8
Andrew Jarrett <ahrjarrett@gmail.com>
2016-12-09 11:52:56 -0700

    2nd lesson done

---
+++ regex/03/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const str = `Cat
+sat on
+the hat.`
+const regex = /.../g
+
+const output = (str, regex, target) => {
+  target.innerHTML =
+    str.replace(regex, (str) =>
+      `<span>${str}</span>`
+    )
+}
+
+output(str, regex, document.querySelector('pre'))
+
