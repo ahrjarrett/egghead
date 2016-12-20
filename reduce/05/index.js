ab50d7c166b65f8f887974206e5c4b41245ce5c3
Andrew Jarrett <ahrjarrett@gmail.com>
2016-12-19 22:35:34 -0700

    added index.js

--- reduce/05/index.js
+++ reduce/05/index.js
@@ -1,4 +1,9 @@
 const data = [[2,3,4], [6,5,7], [9, 10, 11]]
 
+// flattens list to one-dimensional array
+const flattenedData = data.reduce((acc, curr) => {
+  return acc.concat(curr)
+}, [])
 
+console.log(flattenedData)
 
