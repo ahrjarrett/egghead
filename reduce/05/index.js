d02c32dba54c594f4a10abe1662a039da374ddcc
Andrew Jarrett <ahrjarrett@gmail.com>
2016-12-19 22:38:59 -0700

    added input data to 05/index.js

--- reduce/05/index.js
+++ reduce/05/index.js
@@ -1,8 +1,50 @@
 const data = [[2,3,4], [6,5,7], [9, 10, 11]]
 
+var input = [
+  {
+    title: "Batman Begins",
+    year: 2005,
+    cast: [
+      "Christian Bale",
+      "Michael Caine",
+      "Liam Neeson",
+      "Katie Holmes",
+      "Gary Oldman",
+      "Cillian Murphy"
+    ]
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    cast: [
+      "Christian Bale",
+      "Heath Ledger",
+      "Aaron Eckhart",
+      "Michael Caine",
+      "Maggie Gyllenhal",
+      "Gary Oldman",
+      "Morgan Freeman"
+    ]
+  },
+  {
+    title: "The Dark Knight Rises",
+    year: 2012,
+    cast: [
+      "Christian Bale",
+      "Gary Oldman",
+      "Tom Hardy",
+      "Joseph Gordon-Levitt",
+      "Anne Hathaway",
+      "Marion Cotillard",
+      "Morgan Freeman",
+      "Michael Caine"
+    ]
+  }
+]
+
 // flattens list to one-dimensional array
 const flattenedData = data.reduce((acc, curr) => {
-  return acc.concat(curr)
+return acc.concat(curr)
 }, [])
 
 console.log(flattenedData)
