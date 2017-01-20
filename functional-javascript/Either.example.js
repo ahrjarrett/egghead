a8f2dd01f1c43ec653ca334b33c27e6db0387239
Andrew Jarrett <ahrjarrett@gmail.com>
2017-01-19 18:46:19 -0700

    added Either.example, extract Either M into own module

---
+++ functional-javascript/Either.example.js
@@ -0,0 +1,35 @@
+// the code below has not been refactored:
+
+const fs = require('fs')
+
+const Right = (x) =>
+({
+  map: (f) => Right(f(x)),
+  fold: (f, g) => g(x),
+  inspect: () => `Right(${x})`
+})
+
+const Left = (x) =>
+({
+  map: (f) => Left(x),
+  fold: (f, g) => f(x),
+  inspect: () => `Left(${x})`
+})
+
+const fromNullable = (x) =>
+  x != null ? Right(x) : Left(null)
+
+const getPort = () => {
+  try {
+    const str = fs.readFileSync('config.json')
+    const config = JSON.parse(str)
+    return config.port
+  } catch(e) {
+    return 3000
+  }
+}
+
+const result = getPort()
+console.log(result)
+
+
